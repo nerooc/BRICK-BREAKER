e42f76b7df3dcf598df4cd3292de4be7a20a8007
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-03-17 21:34:10 +0100

    added input and paddle
    paddle movement based on event handlers

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import Paddle from '/src/paddle.js';
+import InputHandler from '/src/input.js';
+
+let canvas = document.getElementById("gameScreen");
+
+let ctx = canvas.getContext('2d');
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+//ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+
+paddle.draw(ctx);
+
+new InputHandler(paddle);
+
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+
+    ctx.clearRect(0, 0, 800, 600);
+    paddle.update(deltaTime);
+    paddle.draw(ctx);
+
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
---
+++ src/paddle.js
@@ -0,0 +1,53 @@
+export default class Paddle {
+    constructor(gameWidth, gameHeight) {
+
+        this.gameWidth = gameWidth;
+
+        this.width = 150;
+
+        this.height = 20;
+
+
+        this.maxSpeed = 10;
+
+        this.speed = 0;
+
+        this.position = {
+
+            x: gameWidth / 2 - this.width / 2,
+
+            y: gameHeight - this.height - 10
+
+
+        };
+    }
+
+    moveLeft() {
+        this.speed = -this.maxSpeed;
+    }
+
+    moveRight() {
+        this.speed = this.maxSpeed
+    }
+
+    stop() {
+        this.speed = 0;
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = "red";
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update(deltaTime) {
+        if (!deltaTime) return;
+
+        this.position.x += this.speed;
+
+        if (this.position.x < 0) {
+            this.position.x = 0;
+        } else if (this.position.x > (this.gameWidth - this.width)) {
+            this.position.x = (this.gameWidth - this.width);
+        }
+    }
+}
